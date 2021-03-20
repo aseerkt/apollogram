8368398b2827b02a405244dabfec0e165832d31e
Aseer KT <aseermiqdad@gmail.com>
2021-03-20 18:42:25 +0530

    updated irmconfig to support heroku postgres in prod

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,21 @@
+require('dotenv').confi();
+
+const urlConfig =
+  process.env.NODE_ENV === production
+    ? {
+        url: process.env.DATABASE_URL,
+      }
+    : {
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'insta-clone-1',
+      };
+
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'insta-clone-1',
+  ...urlConfig,
   synchronize: false,
   logging: false,
   entities: ['dist/entities/**/*.js'],
