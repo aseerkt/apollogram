0e63e11e816ce4101b12e2ca4bfebd1a1d6a6c6f
Aseer KT <aseermiqdad@gmail.com>
2021-04-14 10:49:04 +0530

    fixed middleware issue in isUser

--- src/middlewares/isUser.ts
+++ src/middlewares/isUser.ts
@@ -9,18 +9,20 @@ export const isUser: MiddlewareFn<MyContext> = async (
   next
 ) => {
   const token = req.cookies[COOKIE_NAME];
-  console.log({ token });
   if (!token) {
     console.log('No token');
+    return next();
   }
   const { username }: any = verifyToken(token);
 
   if (!username) {
     console.log('No username');
+    return next();
   }
   const user = await User.findOne({ username });
   if (!user) {
     console.log('No user');
+    return next();
   }
   res.locals.username = username;
   return next();
