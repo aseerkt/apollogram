c11e9b184316c39304fb3b6506c8fe151f09c5bd
Aseer KT <aseermiqdad@gmail.com>
2021-05-12 11:06:07 +0530

    fix cookie delete trail 1

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -18,13 +18,12 @@ import {
   RegisterVars,
 } from '../types/userTypes';
 import { formatErrors } from '../utils/formatErrors';
-import { COOKIE_NAME, EXPRESS_ENDPOINT, __prod__ } from '../constants';
+import { COOKIE_NAME, __prod__ } from '../constants';
 import { Profile } from '../entities/Profile';
 import { Post } from '../entities/Post';
 import { createTokenCookie } from '../utils/tokenHandler';
 import { isUser } from '../middlewares/isUser';
 import { isAuth } from '../middlewares/isAuth';
-import { extractDomainFromUrl } from '../utils/extractDomainFromUrl';
 
 @Resolver(User)
 export class UserResolver {
@@ -139,10 +138,7 @@ export class UserResolver {
   @UseMiddleware(isAuth)
   logout(@Ctx() { res }: MyContext) {
     return new Promise((resolve) => {
-      res.clearCookie(COOKIE_NAME, {
-        path: '/',
-        domain: extractDomainFromUrl(EXPRESS_ENDPOINT!),
-      });
+      res.clearCookie(COOKIE_NAME);
       resolve(true);
     });
   }
