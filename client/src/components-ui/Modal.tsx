1174a2f9f68d0a5d2cdb1dd0b7ebfb5d3b0eca8e
Aseer KT <aseermiqdad@gmail.com>
2021-11-18 18:38:15 +0530

    chore: modal close icon

--- client/src/components-ui/Modal.tsx
+++ client/src/components-ui/Modal.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
 import { ModalProps } from '../prop-types/ModalProps';
 
 const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen, children }) => {
@@ -23,12 +22,6 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen, children }) => {
 
   return isOpen ? (
     <div className='fixed top-0 left-0 z-50 w-screen h-screen bg-black bg-opacity-70'>
-      <button
-        className='absolute text-white top-5 right-10'
-        onClick={() => setIsOpen(false)}
-      >
-        <AiOutlineClose size='1.8em' />
-      </button>
       <div
         ref={modalRef}
         className='absolute w-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 md:w-5/12'
