e7cf8b75a8d7f142251bdfe5ee09490ce82faa47
Aseer KT <aseermiqdad@gmail.com>
2021-12-09 10:31:38 +0530

    feat: bring in suspense for code splitting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MeDocument, useMeQuery } from './generated/graphql';
@@ -8,14 +8,14 @@ import Spinner from './components-ui/Spinner';
 import PrivateRoute from './containers/PrivateRoute';
 
 // Routes
-import Login from './routes/Login';
-import Register from './routes/Register';
-import Posts from './routes/Posts';
-import EditProfile from './routes/EditProfile';
-import Profile from './routes/Profile';
-import SinglePost from './routes/SinglePost';
-import MessageProvider from './context/MessageContext';
-import Explore from './routes/Explore';
+const Login = lazy(() => import('./routes/Login'));
+const Register = lazy(() => import('./routes/Register'));
+const Posts = lazy(() => import('./routes/Posts'));
+const EditProfile = lazy(() => import('./routes/EditProfile'));
+const Profile = lazy(() => import('./routes/Profile'));
+const SinglePost = lazy(() => import('./routes/SinglePost'));
+const MessageProvider = lazy(() => import('./context/MessageContext'));
+const Explore = lazy(() => import('./routes/Explore'));
 
 const App: React.FC = () => {
   const { loading, error } = useMeQuery();
@@ -28,21 +28,27 @@ const App: React.FC = () => {
   }
 
   return (
-    <BrowserRouter>
-      <MessageProvider>
-        <div className='pb-10'>
-          <Switch>
-            <PrivateRoute exact path='/' component={Posts} />
-            <PrivateRoute exact path='/explore' component={Explore} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <PrivateRoute exact path='/p/:postId' component={SinglePost} />
-            <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-            <PrivateRoute exact path='/u/:username' component={Profile} />
-          </Switch>
-        </div>
-      </MessageProvider>
-    </BrowserRouter>
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <MessageProvider>
+          <div className='pb-10'>
+            <Switch>
+              <PrivateRoute exact path='/' component={Posts} />
+              <PrivateRoute exact path='/explore' component={Explore} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/p/:postId' component={SinglePost} />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute exact path='/u/:username' component={Profile} />
+            </Switch>
+          </div>
+        </MessageProvider>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
