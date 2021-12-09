e8ac22be3a0b506320cf1188a0820f29bcc4c6a0
Aseer KT <aseermiqdad@gmail.com>
2021-12-09 10:57:06 +0530

    feat: replace craco with vite

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
